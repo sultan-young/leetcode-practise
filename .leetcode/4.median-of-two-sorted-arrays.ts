c8525f3fc7f92e4fec58c418b9f566d3547ffae8
yangzilong <sultany@163.com>
2023-01-03 17:47:40 +0800

    4

--- .leetcode/4.median-of-two-sorted-arrays.ts
+++ .leetcode/4.median-of-two-sorted-arrays.ts
@@ -6,7 +6,15 @@
 
 // @lc code=start
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-
+    const concatArr = nums1.concat(nums2).sort((a, b) => a - b);
+    let len = concatArr.length;
+    if (len % 2 === 1) {
+        return concatArr[Math.floor(len / 2)]
+    } else {
+        let num1 = concatArr[len / 2 - 1];
+        let num2 = concatArr[len / 2];
+        return (num1 + num2) / 2;
+    }
 };
 // @lc code=end
 
