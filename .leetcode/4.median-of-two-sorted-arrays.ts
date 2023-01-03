caa2bd21ed20614a01ecc21cb38c71ccdb9b8d9b
yangzilong <sultany@163.com>
2023-01-03 17:07:57 +0800

    init

---
+++ .leetcode/4.median-of-two-sorted-arrays.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=typescript
+ *
+ * [4] Median of Two Sorted Arrays
+ */
+
+// @lc code=start
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+
+};
+// @lc code=end
+
